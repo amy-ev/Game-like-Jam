9ae4d42931e9194cfd5603522642dea1d4294d64
amy-ev <amyevemy@hotmail.co.uk>
2026-03-01 11:46:36 +0000

    basic implementation of world and player scenes

---
+++ GameLikeJam/player.gd
@@ -0,0 +1,66 @@
+extends CharacterBody3D
+
+@onready var CAMERA_CONTROLLER := $Camera3D
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+const TURN_SPEED = 0.05
+
+# maybe change to be adjustable
+const MOUSE_SENSITIVITY := 0.5
+
+const TILT_LOWER_LIMIT := -1.0
+const TILT_UPPER_LIMIT := 1.0
+
+var _mouse_rotation : Vector3
+var _rotation_input : float
+var _tilt_input : float
+var _player_rotation : Vector3
+var _camera_rotation : Vector3
+
+
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
+	_update_camera(delta)
+
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		_rotation_input = -event.relative.x * MOUSE_SENSITIVITY
+		_tilt_input = -event.relative.y * MOUSE_SENSITIVITY
+
+
+func _update_camera(delta):
+	_mouse_rotation.x += _tilt_input * delta
+	_mouse_rotation.x = clamp(_mouse_rotation.x, TILT_LOWER_LIMIT, TILT_UPPER_LIMIT)
+	_mouse_rotation.y += _rotation_input * delta
+	
+	_player_rotation = Vector3(0.0,_mouse_rotation.y,0.0)
+	_camera_rotation = Vector3(_mouse_rotation.x,0.0,0.0)
+	
+	CAMERA_CONTROLLER.transform.basis = Basis.from_euler(_camera_rotation)
+	CAMERA_CONTROLLER.rotation.z = 0.0
+	
+	global_transform.basis = Basis.from_euler(_player_rotation)
+	
+	# to stop constant spinning
+	_rotation_input = 0.0
+	_tilt_input = 0.0
+	
