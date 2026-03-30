1285e8aa18f1be5623547b34cb797e5ef77c9dc3
amy-ev <amyevemy@hotmail.co.uk>
2026-03-30 13:47:14 +0100

    3d interactive bin assets created

---
+++ GameLikeJam/assets/bin.gd
@@ -0,0 +1,21 @@
+extends Area3D
+
+
+var detected := false
+var opened := false
+
+func _on_body_entered(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		detected = true
+
+func _input(event: InputEvent) -> void:
+	if detected and !opened:
+		if event.is_action_pressed("interact"):
+			if self.name.begins_with("bin"):
+				remove_child(get_node("bin-lid"))
+			opened = true
+			print("opened!")
+
+func _on_body_exited(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		detected = false
