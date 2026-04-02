b3c81c4447bc47314337837cddb53ffd29d47a64
amy-ev <amyevemy@hotmail.co.uk>
2026-04-02 19:42:56 +0100

    head bob implemented

--- GameLikeJam/player/player.gd
+++ GameLikeJam/player/player.gd
@@ -9,9 +9,13 @@ extends CharacterBody3D
 @onready var hitbox = $hitbox/CollisionShape3D
 @onready var stats = $player_stats
 
-const SPEED = 10.0
-const JUMP_VELOCITY = 4.5
+const SPEED = 7.0
 const TURN_SPEED = 0.05
+
+const HEAD_BOB_FREQ = 2.4
+const HEAD_BOB_AMP = 0.08
+var bob_time = 0.0
+var initial_cam_pos: Vector3
 
 # maybe change to be adjustable
 const MOUSE_SENSITIVITY := 0.5
@@ -35,17 +39,19 @@ func _ready():
 	Global.player = self
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	CAMERA_CONTROLLER.make_current()
+	initial_cam_pos = CAMERA_CONTROLLER.transform.origin
+	print(initial_cam_pos)
 	healthChanged.emit(stats.health)
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
+	if not is_on_floor():
+		velocity += get_gravity() * delta
 	if attack_sprite.animation == "scratch":
 		if attack_sprite.frame == 1:
 			hitbox.disabled = true
 			
-	
+
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
@@ -54,16 +60,13 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-	#TODO: REMVOE THIS BEFORE EXPORT
-	if Input.is_action_pressed("jump"):
-		velocity.y = JUMP_VELOCITY
-	elif Input.is_action_pressed("crouch"):
-		velocity.y = -JUMP_VELOCITY
-	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
-	move_and_slide()
-	_update_camera(delta)
+	
+	bob_time += delta * velocity.length() * float(is_on_floor())
+	CAMERA_CONTROLLER.transform.origin = _headbob(bob_time)
 
+	_update_camera(delta)
+	move_and_slide()
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.pressed:
@@ -115,3 +118,10 @@ func _on_hurtbox_area_entered(area: Area3D) -> void:
 			# -= damage_value
 			stats.health -= 10
 			healthChanged.emit(stats.health)
+
+func _headbob(time):
+	var pos = initial_cam_pos
+	pos.y += sin(time * HEAD_BOB_FREQ) * HEAD_BOB_AMP
+	pos.x += cos(time * HEAD_BOB_FREQ/2) * HEAD_BOB_AMP
+	print(pos)
+	return pos
