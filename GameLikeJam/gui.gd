281f56fbd8080165d66cd9da8464e9b1f7bd36db
amy-ev <amyevemy@hotmail.co.uk>
2026-04-03 17:39:46 +0100

    goal tracker implemented

--- GameLikeJam/gui.gd
+++ GameLikeJam/gui.gd
@@ -4,6 +4,8 @@ extends Control
 @onready var level_label = $gui/panel/HBoxContainer/level/VBoxContainer/level
 @onready var lives_label = $gui/panel/HBoxContainer/lives/VBoxContainer/lives
 @onready var health_label = $gui/panel/HBoxContainer/health/VBoxContainer/health
+@onready var baby_1 = $gui/panel/HBoxContainer/goal/HBoxContainer/baby_1
+@onready var baby_2 = $gui/panel/HBoxContainer/goal/HBoxContainer/baby_2
 
 func _ready() -> void:
 	Global.connect("pointsCollected", _on_points_collected)
@@ -12,7 +14,10 @@ func _ready() -> void:
 	lives_label.text = str(Global.lives)
 	points_label.text = str(Global.points)
 	health_label.text = str(Global.player_max_health) + "%"
-	
+	if Global.level >= 2:
+		baby_1.modulate = Color(1.0, 1.0, 1.0, 1.0)
+	if Global.level >= 3:
+		baby_2.modulate = Color(1.0, 1.0, 1.0, 1.0)
 func _on_points_collected(points:int):
 	Global.points += points
 	points_label.text = str(Global.points)
